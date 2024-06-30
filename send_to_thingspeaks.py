f75d125b438e825cbfa58b3f096ea2c7fba457c3
Himanshu-gotwal <152645363+Himanshu-gotwal@users.noreply.github.com>
2024-07-01 02:29:36 +0530

    #esp8266

---
+++ send_to_thingspeaks.py
@@ -0,0 +1,54 @@
+import dht
+import machine
+import network
+import urequests
+import time
+
+# Replace with your Wi-Fi credentials
+SSID = 'HIMANSHU'
+PASSWORD = '88888888'
+
+# ThingSpeak API parameters
+API_KEY = '2587721'
+THINGSPEAK_URL = 'http://api.thingspeak.com/update?api_key=APO2SBC7R1LSKPVO&field1=0'
+
+# DHT11 sensor connected to GPIO 4 (D2)
+dht_pin = machine.Pin(5)
+dht_sensor = dht.DHT11(dht_pin)
+
+# Function to connect to Wi-Fi
+def connect_wifi(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('Connecting to network...')
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            pass
+    print('Network connected:', wlan.ifconfig())
+
+# Function to read data from DHT11
+def read_dht11():
+    dht_sensor.measure()
+    temp = dht_sensor.temperature()
+    hum = dht_sensor.humidity()
+    return temp, hum
+
+# Function to send data to ThingSpeak
+def send_to_thingspeak(temp, hum):
+    response = urequests.get(f'{THINGSPEAK_URL}?api_key={API_KEY}&field1={temp}&field2={hum}')
+    print(response.text)
+    response.close()
+
+# Connect to Wi-Fi
+connect_wifi(SSID, PASSWORD)
+
+# Main loop to read and send data every 15 seconds
+while True:
+    try:
+        temperature, humidity = read_dht11()
+        print(f'Temperature: {temperature}°C, Humidity: {humidity}%')
+        send_to_thingspeak(temperature, humidity)
+    except Exception as e:
+        print('Error:', e)
+    time.sleep(10)
